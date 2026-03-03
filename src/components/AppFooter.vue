9575e833e363dca7526eb67a6e988e3de304bddd
Vova Revenko <vova@revenko.org>
2026-03-03 23:25:09 +0100

    style: use theme-aware heart emoji in footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-caption text-grey q-mt-xl q-mb-md full-width text-center">
-    Сделано с 🤍 для «Fondo Ruso»<br />
+    Сделано с {{ heartEmoji }} для «Fondo Ruso»<br />
     Есть проблемы? Пишите
     <a href="https://t.me/vovarevenko" target="_blank" rel="noopener"
       >@vovarevenko</a
@@ -9,5 +9,10 @@
 </template>
 
 <script setup lang="ts">
+import { useQuasar } from 'quasar';
+import { computed } from 'vue';
+
+const $q = useQuasar();
 const version = process.env.APP_VERSION;
+const heartEmoji = computed(() => ($q.dark.isActive ? '🤍' : '❤️'));
 </script>
