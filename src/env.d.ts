20c580fd6a7a46bd345840a2b7c65fe90bf2715d
Vova Revenko <vova@revenko.org>
2026-03-04 09:54:20 +0100

    chore: add .env to gitignore and create .env.example

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,5 +4,10 @@ declare namespace NodeJS {
     VUE_ROUTER_MODE: 'hash' | 'history' | 'abstract' | undefined
     VUE_ROUTER_BASE: string | undefined
     APP_VERSION: string
+
+    NOCODB_API_URL: string
+    NOCODB_API_TOKEN: string
+    NOCODB_MEMBERS_TABLE_ID: string
+    NOCODB_MEMBERS_VIEW_ID: string
   }
 }
